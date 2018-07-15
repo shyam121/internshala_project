e4b9d040155fb6f06448fbd6fde8b09c56f21b66
shyam121 <reddyshyam1997@gmail.com>
2018-07-15 15:29:29 +0530

    commit 15:29:29.85,Sun 07/15/2018

--- internshala_project/settings.py
+++ internshala_project/settings.py
@@ -37,7 +37,7 @@ with open(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- push.bat
+++ push.bat
@@ -1,4 +1,4 @@
 git init
 git commit -m "commit %time%,%date%"
-git remote add origin https://github.com/shyam121/internshala_project.git
+git remote add -m  origin internshala_project https://github.com/shyam121/internshala_project.git
 git push -u origin master
