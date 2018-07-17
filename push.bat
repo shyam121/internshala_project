a30bc84451cc45a781acb2bd1f707e25eba727e2
shyam121 <reddyshyam1997@gmail.com>
2018-07-17 21:42:07 +0530

    commit 21:42:07.27,Tue 07/17/2018

--- push.bat
+++ push.bat
@@ -3,4 +3,4 @@ git add .
 git commit -m "commit %time%,%date%"
 git remote add -m  origin internshala_project https://github.com/shyam121/internshala_project.git
 git remote -v
-git push origin master
+git push -m origin master
