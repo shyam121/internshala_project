9aa415fe7a0a8ac90e0274fef6d6302bcee0dd10
shyam121 <reddyshyam1997@gmail.com>
2018-07-17 21:16:41 +0530

    commit 21:16:41.63,Tue 07/17/2018

--- internshala_project/settings.py
+++ internshala_project/settings.py
@@ -155,10 +155,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT=os.path.join(BASE_DIR,'statics')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'statics'),
-)
+STATIC_ROOT=os.path.join(BASE_DIR,'statics')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'statics'),
+# )
 
 DEFAULT_FILE_STORAGE = 'db_file_storage.storage.DatabaseFileStorage'
 
