7c80f769769e01b9a06f4e74728c9c761ea5459e
shyam121 <reddyshyam1997@gmail.com>
2018-07-17 21:20:10 +0530

    commit 21:20:10.08,Tue 07/17/2018

--- internshala_project/settings.py
+++ internshala_project/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'statics')
+STATIC_ROOT='statics'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR,'statics'),
 # )
